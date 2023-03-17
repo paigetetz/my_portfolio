9f3f00cf7a2cb26032aecb23eb94c4981268545e
paige <paigetetz@gmail.com>
2023-03-17 19:45:41 -0400

    NavBar/ Project Info

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function LandingPage(props) {
+	const exploreClick = () => {};
 	return (
-		<div>
-			<h1>Landing Page</h1>
-		</div>
+		<Link to='/home' onClick={exploreClick}>
+			<div>
+				<h1>
+					My name is Paige Tetzlaff, and I'm a Full Stack Software Engineer.
+				</h1>
+				<h3>Feel free to explore my work</h3>
+
+				<img
+					src='https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?cs=srgb&dl=pexels-sohail-nachiti-807598.jpg&fm=jpg'
+					alt='Plants'
+				/>
+			</div>
+		</Link>
 	);
 }
 
