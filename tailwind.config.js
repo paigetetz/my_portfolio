4ea4239c77b209875adccbbbc5e98a5a289a17c0
paige <paigetetz@gmail.com>
2023-03-18 14:48:46 -0400

    Tailwind custom palette, navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			sage: '#5f8575',
+			offwhite: '#fcfcfc',
+			cloudy: '#666666',
+			gold: '#e3caab',
+		},
+
 		extend: {},
 	},
 	plugins: [],
