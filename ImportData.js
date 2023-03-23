eaf438d0fa48e1e25540e1c766b1fa79b3084674
paige <paigetetz@gmail.com>
2023-03-23 14:43:35 -0400

    Mongo DB and Express added

---
+++ ImportData.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require('mongodb');
+const fs = require('fs');
+const uri =
+	'mongodb+srv://paigetetz:EqayKBzPhOEds24m@pktportfolio.a2yapa6.mongodb.net/projects'; // Replace <password> with your actual password
+const dbName = 'projects';
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+async function importData() {
+	try {
+		await client.connect();
+
+		const db = client.db(dbName);
+
+		const data = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+
+		for (const [collectionName, documents] of Object.entries(data)) {
+			const collection = db.collection(collectionName);
+			await collection.insertMany(documents);
+			console.log(
+				`Inserted ${documents.length} documents into collection '${collectionName}'`
+			);
+		}
+
+		console.log('Data import completed.');
+	} catch (err) {
+		console.error('Error importing data:', err);
+	} finally {
+		await client.close();
+	}
+}
+
+importData();
